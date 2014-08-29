fc67f1843fc9987afd7d3f6eb2e6d5712e48ce78
DHX <dh9129@hotmail.com>
2014-08-29 19:26:11 +0100

    Third Commit with Zero G Stuff Added

---
+++ scr/main/java/com/DHX/powercraft/block/TestBlock.java
@@ -0,0 +1,13 @@
+package com.DHX.powercraft.block;
+
+import net.minecraft.block.material.Material;
+
+public class TestBlock extends PCBlock
+  {
+    public TestBlock()
+      {
+        super(Material.anvil);
+        this.setBlockName("testblock");
+        this.setBlockTextureName("testblock");
+      }
+   }
