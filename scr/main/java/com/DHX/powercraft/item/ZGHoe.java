87a7ff4af379c77e1254438299ce56ea59a0e17c
DHX <dh9129@hotmail.com>
2014-08-29 19:29:14 +0100

    Third Commit with Zero G Stuff Added

---
+++ scr/main/java/com/DHX/powercraft/item/ZGHoe.java
@@ -0,0 +1,69 @@
+package com.DHX.powercraft.item;
+
+import com.DHX.powercraft.reference.Reference;
+import cpw.mods.fml.common.eventhandler.Event;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemHoe;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.UseHoeEvent;
+
+public class ZGHoe extends ItemHoe
+  {
+    public ZGHoe(Item.ToolMaterial p_i45343_1_)
+      {
+        super(p_i45343_1_);        this.theToolMaterial = p_i45343_1_;
+        this.maxStackSize = 1;
+        this.setMaxDamage(p_i45343_1_.getMaxUses());
+      }
+    @Override
+    public boolean onItemUse(ItemStack p_77648_1_, EntityPlayer p_77648_2_, World p_77648_3_, int p_77648_4_, int p_77648_5_, int p_77648_6_, int p_77648_7_, float p_77648_8_, float p_77648_9_, float p_77648_10_)
+      {
+        if (!p_77648_2_.canPlayerEdit(p_77648_4_, p_77648_5_, p_77648_6_, p_77648_7_, p_77648_1_))
+          {
+            return false;
+          }
+        else
+          {
+            UseHoeEvent event = new UseHoeEvent(p_77648_2_, p_77648_1_, p_77648_3_, p_77648_4_, p_77648_5_, p_77648_6_);
+            if (MinecraftForge.EVENT_BUS.post(event))
+              {
+                return false;
+              }
+
+            if (event.getResult() == Event.Result.ALLOW)
+              {
+                p_77648_1_.damageItem(1, p_77648_2_);
+                return true;
+              }
+
+            Block block = p_77648_3_.getBlock(p_77648_4_, p_77648_5_, p_77648_6_);
+
+            if (p_77648_7_ != 1 && p_77648_3_.getBlock(p_77648_4_, p_77648_5_ + 1, p_77648_6_).isAir(p_77648_3_, p_77648_4_, p_77648_5_ + 1, p_77648_6_) && (block == Blocks.grass || block == Blocks.dirt))
+              {
+                Block block1 = GameRegistry.findBlock(Reference.MOD_ID, "blockzgfarmland");
+                p_77648_3_.playSoundEffect((double)((float)p_77648_4_ + 0.5F), (double)((float)p_77648_5_ + 0.5F), (double)((float)p_77648_6_ + 0.5F), block1.stepSound.getStepResourcePath(), (block1.stepSound.getVolume() + 1.0F) / 2.0F, block1.stepSound.getPitch() * 0.8F);
+
+                if (p_77648_3_.isRemote)
+                  {
+                    return true;
+                  }
+                else
+                  {
+                    p_77648_3_.setBlock(p_77648_4_, p_77648_5_, p_77648_6_, block1);
+                    p_77648_1_.damageItem(1, p_77648_2_);
+                    return true;
+                  }
+              }
+            else
+              {
+                return false;
+              }
+          }
+      }
+  }
